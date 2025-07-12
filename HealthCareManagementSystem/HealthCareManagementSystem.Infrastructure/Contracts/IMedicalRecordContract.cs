f9af65a71d374349ea151f740a89f08bffa0e69f
ManojReyya <manoj.reyya@cognizant.com>
2025-07-12 11:41:18 +0530

    Upload New File

---
+++ HealthCareManagementSystem/HealthCareManagementSystem.Infrastructure/Contracts/IMedicalRecordContract.cs
@@ -0,0 +1,18 @@
+﻿using HealthCareManagementSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareManagementSystem.Infrastructure.Contracts
+{
+    public interface IMedicalRecordContract
+    {
+        Task<MedicalRecord?> GetByMedicalRecordIdAsync(int medicalRecordId);
+        Task<IEnumerable<MedicalRecord>> GetAllMedicalRecordAsync();
+        Task AddMedicalRecordAsync(MedicalRecord record);
+        Task UpdateMedicalRecordAsync(MedicalRecord record);
+        Task DeleteMedicalRecordAsync(int medicalRecordId);
+    }
+}
