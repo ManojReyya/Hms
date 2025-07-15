46c73ff84534f3485a195e1f5a810b9fbc2722cd
ManojReyya <manoj.reyya@cognizant.com>
2025-07-15 07:11:13 +0530

    Manoj added MedicalRecordUpdateDTO

---
+++ HealthCareManagementSystem/HealthCareManagementSystem.Application/DTOs/MedicalRecordUpdateDTO.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareManagementSystem.Application.DTOs
+{
+    public class MedicalRecordUpdateDTO
+    {
+        public int MedicalRecordId { get; set; }
+        public string PatientId { get; set; } = null!;
+        public string DoctorId { get; set; } = null!;
+        public int AppointmentId { get; set; }
+        public string Diagnosis { get; set; } = null!;
+        public string? Prescription { get; set; }
+        public string? Notes { get; set; }
+        public DateTime RecordDate { get; set; }
+    }
+}
