c2ffddff58a8c2367a2a78281cc353c8fc91d18b
ManojReyya <manoj.reyya@cognizant.com>
2025-07-15 07:09:56 +0530

    Manoj added MedicalRecordCreateDTO

---
+++ HealthCareManagementSystem/HealthCareManagementSystem.Application/DTOs/MedicalRecordCreateDTO.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCareManagementSystem.Application.DTOs
+{
+    public class MedicalRecordCreateDTO
+    {
+        public string PatientId { get; set; } = null!;
+        public string DoctorId { get; set; } = null!;
+        public int AppointmentId { get; set; }
+        public string Diagnosis { get; set; } = null!;
+        public string? Prescription { get; set; }
+        public string? Notes { get; set; }
+        public DateTime RecordDate { get; set; }
+    }
+}
